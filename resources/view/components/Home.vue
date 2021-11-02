0470aea175161d44511122933d36a87d9a7cca85
Russel Hussain <md.russel.hussain@gmail.com>
2021-11-02 10:42:47 +0600

    Element plus working as expected

--- resources/view/components/Home.vue
+++ resources/view/components/Home.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Hello from Vue</h1>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="User" name="first">User</el-tab-pane>
+    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
   export default {
-    name: 'Home'
+    name: 'Home',
+    setup(){
+      const handleClick = function (tab, event) {
+        console.log(tab, event)
+      }
+
+      return {
+        handleClick
+      }
+    }
   }
 </script>
--- resources/view/main.js
+++ resources/view/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import router from './router';
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './components/App.vue';
+
 const app = createApp(App);
 app.use(router);
+app.use(ElementPlus)
 
 app.mount('#quiz_app');
